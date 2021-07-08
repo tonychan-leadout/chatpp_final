8eb004abdc6a41efdf3806062f52698179357ee0
tonychan-leadout <84704581+tonychan-leadout@users.noreply.github.com>
2021-07-08 12:07:27 +0800

    Update conversation.dart

--- lib/conversation.dart
+++ lib/conversation.dart
@@ -1,169 +1,70 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'conversation_detail.dart';
-import 'dart:io';
-import 'dart:convert';
-import 'package:http/http.dart' ;
-class friends{
 
-  String name;
-  String text;
-  friends({required this.name,required this.text});
-}
-class ChatUsers{
+import 'package:flutter/material.dart';
+import 'chat.dart';
+class ConversationList extends StatefulWidget{
+  String id;
   String name;
   String messageText;
-  String imageURL;
+  String imageUrl;
   String time;
-  ChatUsers({required this.name,required this.messageText,required this.imageURL,required this.time});
-}
-
-class ChatPage extends StatefulWidget {
-  ChatPage({ required this.id}) ;
-  final String id;
-  List<friends> Friends=[];
+  bool isMessageRead;
+  ConversationList({required this.id,required this.name,required this.messageText,required this.imageUrl,required this.time,required this.isMessageRead});
   @override
-  _ChatPageState createState() => _ChatPageState();
+  _ConversationListState createState() => _ConversationListState();
 }
 
-class _ChatPageState extends State<ChatPage> {
-  List<ChatUsers> Friends=[];
-
-  void initState() {
-    super.initState();
-    getmymsg();
-  }
+class _ConversationListState extends State<ConversationList> {
   @override
   Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print(widget.name);
+        Navigator.push(
+            context, MaterialPageRoute(
+          builder: (context) => ChatPage(id: widget.id, sendid: widget.name,),
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        )
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
+        child: Row(
           children: <Widget>[
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.only(left: 16,right: 16,top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text("Chat",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
-                    Container(
-                      padding: EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
-                      height: 30,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.deepPurple[200],
-                      ),
-                      child: Row(
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: ExactAssetImage('images/user.png'),
+                    maxRadius: 30,
+                  ),
+                  SizedBox(width: 16,),
+                  Expanded(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Icon(Icons.add,color: Colors.deepPurple,size: 20,),
-                          SizedBox(width: 2,),
-                          Text("Add New",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                          Text(widget.name, style: TextStyle(fontSize: 16),),
+                          SizedBox(height: 6,),
+                          Text(widget.messageText, style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.grey.shade600,
+                              fontWeight: widget.isMessageRead
+                                  ? FontWeight.normal
+                                  : FontWeight.normal),),
                         ],
                       ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.only(top: 16,left: 16,right: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Search...",
-                  hintStyle: TextStyle(color: Colors.grey.shade600),
-                  prefixIcon: Icon(Icons.search,color: Colors.grey.shade600, size: 20,),
-                  filled: true,
-                  fillColor: Colors.grey.shade100,
-                  contentPadding: EdgeInsets.all(8),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(
-                          color: Colors.grey.shade100
-                      )
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
-            ListView.builder(
-              itemCount: Friends.length,
-              shrinkWrap: true,
-              padding: EdgeInsets.only(top: 16),
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index){
-                return ConversationList(
-                  id: widget.id,
-                  name: Friends[index].name,
-                  messageText: Friends[index].messageText,
-                  imageUrl: Friends[index].imageURL,
-                  time: Friends[index].time,
-                  isMessageRead: (index == 0 || index == 3)?true:false,
-                );
-              },
-            ),
+            Text(widget.time, style: TextStyle(fontSize: 12,
+                fontWeight: widget.isMessageRead ? FontWeight.normal : FontWeight
+                    .normal),),
           ],
         ),
       ),
     );
-  }
-  Future getmymsg() async {
-    final url = Uri.parse(_localhostss());
-    //Response response = await get(url);
-    //print(response.body);
-    final json = '{"id":"${widget.id}"}';
-    final response = await post(Uri.parse(_localhostss()), headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    }, body: json);
-    // print('Status code: ${response.statusCode}');
-    List <dynamic>data= jsonDecode(response.body);
-    print( jsonDecode(response.body));
-    data.reversed.toList().forEach((element) {
-      setState(() {
-        if (Friends.length ==0){
-          if(element['name'] == widget.id){
-            Friends = [...Friends, ChatUsers(name: element['receiver'], messageText: ('✓✓ ${element['message']}'), imageURL: 'imageURL', time: 'Now')];
-          }
-          else{
-            Friends = [...Friends, ChatUsers(name: element['name'], messageText: element['message'], imageURL: 'imageURL', time: 'Now')];
-          }
-        }
-        else{
-          int count=0;
-          Friends.forEach((item) {
-            if (element['name'] == item.name || element['receiver'] == item.name){
-              print('existed');
-            }
-            else{
-              count=count+1;
-              if(count == Friends.length){
-                if(element['name'] == widget.id){
-                  Friends = [...Friends, ChatUsers(name: element['receiver'], messageText: ('✓✓ ${element['message']}'), imageURL: 'imageURL', time: 'Now')];
-                }
-                else{
-                  Friends = [...Friends, ChatUsers(name: element['name'], messageText: element['message'], imageURL: 'imageURL', time: 'Now')];
-                }
-              }
-            }
-          });
-
-        }
-      });
-    });
-    /*data.forEach((element) {
-        setState(() {
-          Friends = [...Friends, ChatUsers(name: name, messageText: messageText, imageURL: imageURL, time: time)]
-        });
-            },
-    );*/
-
-  }
-  String _localhostss() {
-    if (Platform.isAndroid)
-      return 'http://192.168.1.26:7878/getuser';
-    else // for iOS simulator
-      return 'http://192.168.1.26:7878/getuser';
   }
 }
